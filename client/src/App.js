f57f57f3fb93fa1c9f7a3718e4a51dab8506915b
Paul Lujan <paul.lujan@smilebrands.com>
2020-11-24 10:01:52 -0800

    Fixed tests

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn how to pirouette
+          Learn React 
         </a>
       </header>
     </div>
